cbc5d8819e570d73147ac7acd178ba371e5b5fd3
alihfph <ali.hfph@gmail.com>
2021-03-30 18:36:38 +0200

    initial commit

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Test from './component/first';
+
+function App() {
+  return (
+    <div className="App">
+    <Test />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/component/first.jsx
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react'
+
+
+class Test extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <h1> this is my first line</h1>
+      </div>
+     );
+  }
+
+
+}
+ 
+export default Test
